ef4c632d3dde558dff7a40003886c3dd53e7b2bc
Connor Browder <connorbrowder@wustl.edu>
2022-10-16 15:07:42 -0500

    commit

--- find_my_fit/resources/views/home.blade.php
+++ find_my_fit/resources/views/home.blade.php
@@ -4,16 +4,39 @@
     <title>Home</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-    <link rel="stylesheet" type="text/css" href="https://classes.engineering.wustl.edu/cse330/content/weather.css">
     <style>
     .w3-sidebar a {font-family: "Roboto", sans-serif}
     body,h1,h2,h3,h4,h5,h6,.w3-wide {font-family: "Montserrat", sans-serif;}
   </style>
 </head>
-<body>
+<body class="w3-content" style="max-width:1200px">
+  <div>
+    <nav class="w3-sidebar w3-bar-block w3-white w3-collapse w3-top" style="z-index:3;width:250px" id="mySidebar">
+      <div class="w3-container w3-display-container w3-padding-16">
+        <i onclick="w3_close()" class="fa fa-remove w3-hide-large w3-button w3-display-topright"></i>
+        <h3 class="w3-wide"><b>FindMyFit</b></h3>
+      </div>
+    </nav>
+    <div class="w3-display-container w3-green" style="height:100px;">
+      <div class="w3-display-topleft">Top Left</div>
+      <div class="w3-display-topright">Top Right</div>
+      <div class="w3-display-topmiddle">Top Mid</div>
+    </div>
+</div>
+  <!-- <nav class="w3-white">
+    <div class="w3-bar-item w3-container w3-display-container w3-padding-16">
+      <h3 class="w3-wide"><b>FindMyFit</b></h3>
+    </div>
+    <div class="w3-padding-16">
+      <a href="wardrobe" class="w3-button">Wardrobe</a>
+    </div>
+  </nav> -->
 
-  <div id="weather">
+  <div class="w3-container "id="weather" >
       <div id="description"></div>
       <h1 id="temp"></h1>
       <div id="location"></div>
