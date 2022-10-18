3e1b69842a82baa9e57d8572276ba4bb0e464c6d
Connor Browder <connorbrowder@wustl.edu>
2022-10-18 16:59:04 -0500

    navbar

--- find_my_fit/resources/views/home.blade.php
+++ find_my_fit/resources/views/home.blade.php
@@ -14,81 +14,23 @@
   </style>
 </head>
 <body class="w3-content" style="max-width:1200px">
-  <div>
     <nav class="w3-sidebar w3-bar-block w3-white w3-collapse w3-top" style="z-index:3;width:250px" id="mySidebar">
-      <div class="w3-container w3-display-container w3-padding-16">
-        <i onclick="w3_close()" class="fa fa-remove w3-hide-large w3-button w3-display-topright"></i>
+      <div class="w3-dropdown-hover w3-container w3-display-container w3-padding-16" style="background-color:white;">
         <h3 class="w3-wide"><b>FindMyFit</b></h3>
+        <div class="w3-dropdown-content">
+          <a href="wardrobe" class="w3-button">My Wardrobe</a>
+        </div>
       </div>
     </nav>
-    <div class="w3-display-container w3-green" style="height:100px;">
-      <div class="w3-display-topleft">Top Left</div>
-      <div class="w3-display-topright">Top Right</div>
-      <div class="w3-display-topmiddle">Top Mid</div>
+    <div class="w3-main" style="margin-left:250px">
+      <div class="w3-hide-large" style="margin-top:83px"></div>
+        <header class="w3-container w3-xlarge">
+          <p class="w3-left">MyOutfit</p>
+        </header>
     </div>
-</div>
-  <!-- <nav class="w3-white">
-    <div class="w3-bar-item w3-container w3-display-container w3-padding-16">
-      <h3 class="w3-wide"><b>FindMyFit</b></h3>
-    </div>
-    <div class="w3-padding-16">
-      <a href="wardrobe" class="w3-button">Wardrobe</a>
-    </div>
-  </nav> -->
-
-  <div class="w3-container "id="weather" >
-      <div id="description"></div>
-      <h1 id="temp"></h1>
-      <div id="location"></div>
-  </div>
-
-  <p>Click the button to get your coordinates.</p>
-
-  <button type="submit" id="loc" onclick="getLocation()">Try It</button>
-
-  <p id="demo"></p>
-
-  <script lang="text/javascript">
-    var x = document.getElementById("demo");
-    function getLocation() {
-      event.preventDefault(); 
-      if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition);
-      } else { 
-        x.innerHTML = "Geolocation is not supported by this browser.";
-      }
-    }
 
 
-    function showPosition(position) {
-      x.innerHTML = "Latitude: " + position.coords.latitude + 
-      "<br>Longitude: " + position.coords.longitude;
-      long = position.coords.longitude
-      lat = position.coords.latitude
-      
-    
-      var key = '4b8e7bbc342f87a6e521860430a27b2b';
-      fetch('https://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + long +'&appid=' + key)  
-      .then(function(resp) { return resp.json() }) // Convert data to json
-      .then(function(data) {
 
-        drawWeather(data)
-
-      })
-    }
-  
-
-    function drawWeather( d ) {
-      //var celcius = Math.round(parseFloat(d.main.temp)-273.15);
-      var fahrenheit = Math.round(((parseFloat(d.main.temp)-273.15)*1.8)+32); 
-      
-      document.getElementById('description').innerHTML = d.weather[0].description;
-      document.getElementById('temp').innerHTML = fahrenheit + '&deg;';
-      document.getElementById('location').innerHTML = d.name;
-    }
- 
-   
-  </script>
 
 </body>
 </html>
--- find_my_fit/resources/views/wardrobe.blade.php
+++ find_my_fit/resources/views/wardrobe.blade.php
@@ -18,9 +18,11 @@ body,h1,h2,h3,h4,h5,h6,.w3-wide {font-family: "Montserrat", sans-serif;}
 
 <!-- Sidebar/menu -->
 <nav class="w3-sidebar w3-bar-block w3-white w3-collapse w3-top" style="z-index:3;width:250px" id="mySidebar">
-  <div class="w3-container w3-display-container w3-padding-16">
-    <i onclick="w3_close()" class="fa fa-remove w3-hide-large w3-button w3-display-topright"></i>
+  <div class="w3-dropdown-hover w3-container w3-display-container w3-padding-16" style="background-color:white;">
     <h3 class="w3-wide"><b>FindMyFit</b></h3>
+    <div class="w3-dropdown-content">
+        <a href="home" class="w3-button">My Oufit</a>
+     </div>
   </div>
   <div class="w3-padding-64 w3-large w3-text-grey" style="font-weight:bold">
     <a href="#" class="w3-bar-item w3-button">All</a>
