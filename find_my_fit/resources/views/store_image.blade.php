3cde6d404de3dbbbc104768a13c456d554a4533d
Connor Browder <connorbrowder@wustl.edu>
2022-11-14 15:40:07 -0600

    ui changes pt3

--- find_my_fit/resources/views/store_image.blade.php
+++ find_my_fit/resources/views/store_image.blade.php
@@ -1,122 +1,176 @@
 
+<!DOCTYPE html>
 <html>
 <head>
- <meta name="viewport" content="width=device-width, initial-scale=1">
- <title>Insert Image into Wardrobe</title>
- <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
- <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-</head>
-<body>
- <div class="container">    
-  <br />
-  <button type="button" class="w3-large" onclick="window.location='/'">Home</button>
-  <h3 align="center">Insert Image into Wardrobe</h3>
-    <br />
-    @if($errors->any())
-    <div class="alert alert-danger">
-           <ul>
-           @foreach($errors->all() as $error)
-            <li>{{ $error }}</li>
-           @endforeach
-           </ul>
-        </div>
-    @endif
+    <title>Add Clothing</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-kQtW33rZJAHjgefvhyyzcGF3C5TFyBQBA13V1RKPf4uH+bwyzQxZ6CmMZHmNBEfJ" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <style>
 
-    @if(session()->has('success'))
-     <div class="alert alert-success">
-     {{ session()->get('success') }}
-     </div>
-    @endif
-    <br />
+body,h1,h2,h3,h4,h5,h6,.w3-wide {
+  font-family: "Montserrat", sans-serif;
+}
 
-    <div class="panel panel-default">
-         <div class="panel-heading">
-             <h3 class="panel-title">User Form</h3>
-         </div>
-         <div class="panel-body">
-         <br />
-         <form method="post" action="{{ url('store_image/insert_image') }}"  enctype="multipart/form-data">
-          @csrf
-          <div class="form-group">
-          <div class="row">
-           <label class="col-md-4" align="right">Enter Info</label>
-           <div class="col-md-8">
-            <input type="text" name="user_name" class="form-control" />
-           </div>
+    </style>
+
+<div class="container-fluid">
+      <div class="row flex-nowrap">
+          <div class="min-vh-100 col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-light sticky-top">
+             <div class="d-flex flex-sm-column flex-row flex-nowrap bg-light align-items-center sticky-top">
+                <a class="d-block p-3 link-dark text-decoration-none">
+                  <span id="logo" class="text-secondary fs-2 w3-wide d-none d-sm-inline">FindMyFit</span>
+                </a>
+                <ul class="nav nav-pills nav-flush flex-sm-column flex-row flex-nowrap mb-auto mx-auto text-center align-items-center" id="menu">
+                  <li class="nav-item">
+                    <a href="/wardrobe" class="text-info nav-link align-middle py-3 px-2">
+                        <i class="fs-2 bi-house"></i> <span class="fs-4 ms-1 d-none d-sm-inline">Wardrobe</span>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="/test" class="text-info nav-link py-3 px-2 align-middle">
+                      <i class="fs-2 bi-table"></i> <span class="fs-4 ms-1 d-none d-sm-inline">Fit Me</span></a>
+                  </li>
+                  
+                </ul>
+                
+                <div class="position-fixed pb-4" style="bottom: 0px">
+                  <a href="#" class=" text-secondary d-flex align-items-center text-decoration-none dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    <i class="fs-2 bi-person-circle"></i>
+                    <span class="d-none d-sm-inline mx-1">Username</span>
+                  </a>
+                  <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="/account">Profile</a></li>
+                    <li><form class="dropdown-item text-decoration-none" method="POST" action="{{ route('logout') }}">
+                        @csrf
+
+                        <x-responsive-nav-link :href="route('logout')"
+                        onclick="event.preventDefault(); this.closest('form').submit();">
+                            {{ __('Log Out') }}
+                        </x-responsive-nav-link>
+                    </form>
+                            {{-- @endauth --}}
+                        {{-- </div> --}}
+                    {{-- @endif --}}
+                    </li>
+                  </ul>
+                </div>
+              </div>
           </div>
-         </div>
+        <div class="col py-3">
+            <div class="container">    
+            <br />
+            <h3 align="center">Insert Image into Wardrobe</h3>
+                <br />
+                @if($errors->any())
+                <div class="alert alert-danger">
+                    <ul>
+                    @foreach($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                    </ul>
+                    </div>
+                @endif
 
-         <div class="form-group">
-          <div class="row">
-           <label class="col-md-4" align="right">Select Image</label>
-           <div class="col-md-8">
-            <input type="file" name="user_image" />
-           </div>
-          </div>
-         </div>
+                @if(session()->has('success'))
+                <div class="alert alert-success">
+                {{ session()->get('success') }}
+                </div>
+                @endif
+                <br />
 
-         <div class="form-group">
-         <div class="row">
-            <label class="col-md-4" align="right">Choose a type:</label>
-            <div class="col-md-8">
-                <input type="radio" name="type" value="Innerwear">
-                <label>Innerwear</label><br>
-                <input type="radio" name="type" value="Outterwear">
-                <label>Outterwear</label><br>
-                <input type="radio" name="type" value="Bottom">
-                <label>Bottom</label><br>
-                <input type="radio" name="type" value="Shoes">
-                <label>Shoes</label>
+                <div class="panel panel-default">
+                    
+                    <div class="panel-body">
+                        <br />
+                        <form method="post" action="{{ url('store_image/insert_image') }}"  enctype="multipart/form-data">
+                        @csrf
+                        <div class="form-group">
+                            <div class="row">
+                            <label class="col-md-4" align="right">Enter Info</label>
+                                <div class="col-md-8">
+                                    <input type="text" name="user_name" class="form-control" />
+                                </div>
+                            </div>
+                        </div>
+                        <br />
+                        <div class="form-group">
+                            <div class="row">
+                            <label class="col-md-4" align="right">Select Image</label>
+                                <div class="col-md-8">
+                      
+                                    <!-- <input type="file" name="user_image"/> -->
+                                    <div class="mb-3">
+                                        <input class="form-control" name="user_image" type="file" id="formFile">
+                                    </div>
+                      
+                                    
+                           
+                            </div>
+                        </div>
+                        <br />
+                        
+                        <div class="form-group">
+                            <div class="row">
+                                <label class="col-md-4" align="right">Choose a type:</label>
+                                <div class="col-md-8">
+                                    <input class="form-check-input" type="radio" name="type" value="Innerwear">
+                                    <label class="form-check-label">Innerwear</label><br>
+                                    <input class="form-check-input" type="radio" name="type" value="Outterwear">
+                                    <label class="form-check-label">Outterwear</label><br>
+                                    <input class="form-check-input" type="radio" name="type" value="Bottom">
+                                    <label class="form-check-label">Bottom</label><br>
+                                    <input class="form-check-input" type="radio" name="type" value="Shoes">
+                                    <label class="form-check-label">Shoes</label>
+                                </div>
+                            </div>  
+                        </div>
+                        <br />
+                        <div class="form-group">
+                        <div class="row">
+                            <label class="col-md-4" align="right">Choose a color:</label>
+                            <div class="col-md-8">
+                                <input type="color" class="form-control form-control-color" id="color" name="color" value="#ff0000"><br><br>
+                            </div>
+                        </div>  
+                        </div>
+                        <br />
+
+                        <div class="form-group" align="center">
+                        <br />
+                        <br />
+            
+                        <input type="submit" name="store_image" class="btn btn-primary" value="Upload" />
+                        
+                        </div>
+                        </form>
+                </div>
+                </div>
+                <div class="row row-cols-1 row-cols-sm-3 g-4 m-4">
+                @foreach($data as $row)
+                <div class="col">
+                
+                  <div class="card">
+                    
+                    <img src="store_image/fetch_image/{{ $row->id }}" class="card-img-top" height=200>
+                    <div class="card-body">
+                      <h5 class="card-title">{{ $row->user_name }}</h5>
+                      <p class="card-text">{{ $row->type }}</p>
+                      
+                    </div>
+                    
+                  </div>
+                  
+                </div>
+                @endforeach
+                
+
+              </div> 
             </div>
-        </div>  
         </div>
-
-        <div class="form-group">
-         <div class="row">
-            <label class="col-md-4" align="right">Choose a color:</label>
-            <div class="col-md-8">
-                <input type="color" id="color" name="color" value="#ff0000"><br><br>
-            </div>
-        </div>  
-        </div>
-        
-         <div class="form-group" align="center">
-          <br />
-          <br />
-          <button type="button" class="w3-large" onclick="window.location='/wardrobe'">Back</button>
-          <input type="submit" name="store_image" class="btn btn-primary" value="Upload" />
-          
-         </div>
-         </form>
-      </div>
-     </div>
-     <div class="panel panel-default">
-         <div class="panel-heading">
-             <h3 class="panel-title">User Data</h3>
-         </div>
-         <div class="panel-body">
-         <div class="table-responsive">
-                <table class="table table-bordered table-striped">
-                  <tr>
-                     <th width="30%">Image</th>
-                     <th width="10%">Type</th>
-                     <th width="60%">Name</th>
-                  </tr>
-                  @foreach($data as $row)
-                  <tr>
-                   <td>
-                    <img src="store_image/fetch_image/{{ $row->id }}"  class="img-thumbnail" width="75" />
-                   </td>
-                   <td>{{ $row->type }}</td>
-                   <td>{{ $row->user_name }}</td>
-                  </tr>
-                  @endforeach
-              </table>
-              {!! $data->links() !!}
-             </div>
-         </div>
-     </div>
-    </div>
- </body>
+</div>
+</body>
 </html>
