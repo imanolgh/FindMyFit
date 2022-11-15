1df028fe89796145c321dc52cff34c587c4215e6
Connor Browder <connorbrowder@wustl.edu>
2022-11-15 17:02:19 -0600

    social

--- find_my_fit/resources/views/social.blade.php
+++ find_my_fit/resources/views/social.blade.php
@@ -92,6 +92,21 @@ body,h1,h2,h3,h4,h5,h6,.w3-wide {
                 
 
               </div> 
+              <div>
+                @foreach($social_data as $person)
+                    <div>{{$person->name}}</div>
+                    <div>{{$person ->email}}</div>
+                    <div class="container">
+                        <form method="post" action="{{ route('get_other_account') }}"
+                            enctype="multipart/form-data">
+                            @csrf
+                            <input type='hidden' name='user_id' value='{{ $person->id}}'>
+                            <input type="submit" value="view account" >
+            
+                        </form>
+                    </div>
+                @endforeach
+            </div>
               
   
 
@@ -100,19 +115,5 @@ body,h1,h2,h3,h4,h5,h6,.w3-wide {
 </div>
         
 =======
-<div>
-    @foreach($social_data as $person)
-        <div>{{$person->name}}</div>
-        <div>{{$person ->email}}</div>
-        <div class="container">
-            <form method="post" action="{{ route('get_other_account') }}"
-                enctype="multipart/form-data">
-                @csrf
-                <input type='hidden' name='user_id' value='{{ $person->id}}'>
-                <input type="submit" value="view account" >
- 
-            </form>
-        </div>
-    @endforeach
-</div>
+
 >>>>>>> main
